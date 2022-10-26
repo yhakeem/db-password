3ae7e87c9da7c191900359073591ec99b0eaefeb
stevenjlance <steven.j.lance@gmail.com>
2022-10-26 17:49:29 -0400

    added CR files

---
+++ index.js
@@ -0,0 +1,17 @@
+const User = require('./User.js');
+const bcrypt = require('bcrypt');
+
+const user = {
+    username: 'albert',
+    password: 'bertie99'
+};
+
+// YOUR CODE
+
+
+
+// DO NOT EDIT BELOW
+module.exports = {
+    register,
+    login
+};
---
+++ sequelize.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'db.sqlite')
+});
+
+// DO NOT EDIT BELOW
+
+module.exports = sequelize;
